fb098888aafbb00248d9a1bed028b31d6979c0a3
Abhishek Kumar Singh <151938517+AK-S88@users.noreply.github.com>
2025-04-04 16:22:48 -0400

    Create config.py

---
+++ scraping/config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# URL to scrape
+URL = "https://www.uml.edu/msb/departments/operations-info-systems/programs/msba.aspx"
+
+# Delay to allow full page load (seconds)
+DELAY_SECONDS = 3
+
+# Output CSV path
+OUTPUT_CSV = "data_scraper/scraped_data.csv"
